f39bea9def4332d2db17165a697c2b1317fad632
Andre Rother <andre@andrerother.de>
2019-01-26 21:26:19 +0100

    Implementation of placeholder assets and player movement

---
+++ Assets/Movement.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Rigidbody2D rb2d;
+    public float speed = 50f;
+
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+
+        HandleMovement(horizontal);
+
+
+        //if (Input.GetKey("d"))
+        //{
+        //    rb2d.AddForce(new Vector2(forwardForce * Time.deltaTime, 0f));
+        //}
+
+        //if (Input.GetKey("a"))
+        //{
+        //    rb2d.AddForce(new Vector2(-forwardForce * Time.deltaTime, 0f));
+        //}
+
+        //rb2d.AddForce(Vector2.zero);
+    }
+
+    private void HandleMovement(float horizontal)
+    {
+        rb2d.velocity = new Vector2(horizontal * speed, rb2d.velocity.y);
+    }
+}

3b2303385db64c237442b62ddbff959241c2d837
Andre Rother <andre@andrerother.de>
2019-01-26 23:07:02 +0100

    Automatic moving object in a scene

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public Rigidbody2D rb2d;
+    private const int ENEMY_MOVING_LENGTH_VALUE = 30;
+    public float speed = 50f;
+    public float movingDirection = 1;
+    public int enemyMovementLength = ENEMY_MOVING_LENGTH_VALUE;
+
+    void FixedUpdate()
+    {
+        HandleMovement(movingDirection);
+
+        enemyMovementLength -= 1;
+        if(enemyMovementLength == 0)
+        {
+            movingDirection *= -1;
+            enemyMovementLength = ENEMY_MOVING_LENGTH_VALUE;
+        }
+
+    }
+
+    private void HandleMovement(float horizontal)
+    {
+        rb2d.velocity = new Vector2(horizontal * speed, rb2d.velocity.y);
+    }
+}

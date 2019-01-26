c54355940c7000a9081e9ae577c0f60ad36e8dc4
Digi951 <47057294+Digi951@users.noreply.github.com>
2019-01-26 23:51:32 +0100

    Collect and Drop Objects
    Version 1 -> Collect Tree

---
+++ Assets/CollectObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectObject : MonoBehaviour
+{
+    public GameObject currentInterObject = null;
+    public Collider2D collider;
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("objectTree"))
+        {
+            currentInterObject = collider.gameObject;
+        }
+
+
+        if (collider.CompareTag("objectBarrage"))
+        {
+            currentInterObject = null;
+        }
+    }
+}

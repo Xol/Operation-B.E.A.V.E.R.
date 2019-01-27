501d1a667500156aabbeede39f6f669189057e14
Andre Rother <andre@andrerother.de>
2019-01-27 03:08:43 +0100

    Dynamic spawn of enemies and bushes

--- Assets/EnemyMovement.cs
+++ Assets/EnemyMovement.cs
@@ -1,30 +1,33 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
     public Rigidbody2D rb2d;
     private const int ENEMY_MOVING_LENGTH_VALUE = 30;
+    public int enemyMovementLength = ENEMY_MOVING_LENGTH_VALUE;
     public float speed = 50f;
     public float movingDirection = 1;
-    public int enemyMovementLength = ENEMY_MOVING_LENGTH_VALUE;
 
     void FixedUpdate()
     {
-        HandleMovement(movingDirection);
+        handleMovement(movingDirection);
 
         enemyMovementLength -= 1;
-        if(enemyMovementLength == 0)
-        {
-            movingDirection *= -1;
-            enemyMovementLength = ENEMY_MOVING_LENGTH_VALUE;
-        }
 
+            if (enemyMovementLength == 0)
+            {
+                movingDirection *= -1;
+                enemyMovementLength = ENEMY_MOVING_LENGTH_VALUE;
+            }
     }
 
-    private void HandleMovement(float horizontal)
+    private void handleMovement(float horizontal)
     {
         rb2d.velocity = new Vector2(horizontal * speed, rb2d.velocity.y);
+    }
+
+    public void setMovingDirection(int direction)
+    {
+        this.movingDirection = direction;
     }
 }
---
+++ Assets/MapAttributes.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class MapAttributes : MonoBehaviour
+{
+    public Transform bush;
+    public Transform wolf;
+    private List<Transform> bushs = new List<Transform>();
+    private List<Transform> wolfs = new List<Transform>();
+    private int[] bushPrefabPositions = { -13, 0, 15 };
+
+    private const int WAIT_FOR_ENEMY = 60;
+    public int waitForEnemy = WAIT_FOR_ENEMY;
+
+    private void Start()
+    {
+        for (int i = 0; i < bushPrefabPositions.Length; i++)
+        {
+            bushs.Add(spawnPrefab(bushPrefabPositions[i], bush));
+        }
+    }
+
+    void FixedUpdate()
+    {
+        waitForEnemy -= 1;
+
+        if (waitForEnemy == 0)
+        {
+            if(wolfs.Count > 0)
+            {
+                Destroy(wolfs[0].gameObject);
+                wolfs.RemoveAt(0);
+            }
+
+            wolfs.Add(spawnPrefab(bushPrefabPositions[getRandomArrayPos(bushPrefabPositions.Length)], wolf));
+            waitForEnemy = WAIT_FOR_ENEMY;
+
+        }
+    }
+
+    private int getRandomArrayPos(int length)
+    {
+        System.Random generator = new System.Random();
+        int randomArrayPos = generator.Next(0, length);
+
+        return randomArrayPos;
+    }
+
+    private Transform spawnPrefab(int posX, Transform tType)
+    {
+        return Instantiate(tType, new Vector2(posX, -1.8f), Quaternion.identity);
+    }
+}

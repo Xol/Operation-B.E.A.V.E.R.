31c3ff0bc0f930dd9d83ee31495d702ad6984c54
Digi951 <47057294+Digi951@users.noreply.github.com>
2019-01-27 01:56:41 +0100

    Add Highscore without showing on Display

--- Assets/CollectObject.cs
+++ Assets/CollectObject.cs
@@ -1,11 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CollectObject : MonoBehaviour
 {
     public GameObject currentInterObject = null;
     public Collider2D collider;
+
+    public Text highscore;
+    public int highscoreInt;
+
+
+    void Start()
+    {
+        highscoreInt = 0;        
+    }
 
     void OnTriggerEnter2D(Collider2D collider)
     {
@@ -15,9 +25,18 @@ public class CollectObject : MonoBehaviour
         }
 
 
-        if (collider.CompareTag("objectBarrage"))
-        {
+        if (collider.CompareTag("objectBarrage") && currentInterObject != null)
+        {       
             currentInterObject = null;
+
+            highscoreInt++;
+
+
+            Debug.Log(highscoreInt.ToString());
+            Debug.Log(currentInterObject);
+
+            highscore.text = "TREFFER";
+
         }
     }
 }
